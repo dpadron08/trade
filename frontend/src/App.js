1110415c3eea9e34b00acc5be9a75822dcf2abec
David Padron Castillo <padroncastillo@gmail.com>
2022-04-14 16:08:35 -0400

    Create navbar and navigation using reactroutes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import react from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Map from "./pages/map";
+import About from "./pages/about";
 
 function App() {
   return (
     <div className="App">
-      Hello, world. TRADE!
+      <BrowserRouter>
+      <div className="container">
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" className="navbar-brand">
+          TRADE
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Map
+            </Link>
+          </li>
+          <li className="nav-item" >
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+        </div>
+      </nav>
+        <Routes>
+          <Route path="/" element={<Map text="map text"/>}/> 
+          <Route path="/about" element={<About text="about text"/>}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/map.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Map(props) {
+
+  return (<h1>Hello {props.text}</h1>);
+}
+
+export default Map;
