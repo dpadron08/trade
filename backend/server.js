61959c1b4284f276a986bc191f1dfed7b000ad30
David Padron Castillo <padroncastillo@gmail.com>
2022-04-20 21:04:04 -0400

    Set up basic controller, routes, and models for database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const viewerRouter = require("./routes/viewer.router");
+
 dotenv.config();
 
 const app = express();
@@ -15,6 +17,15 @@ mongoose.connect(process.env.URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", () => console.log("database connection failed"));
 db.once("open", () => console.log("database connection successful"));
+
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.send(
+    'Welcome to the Trade backend, please send requests to "api/viewer/{object}/{mtl|obj|jpg}"'
+  );
+});
+
+app.use("/api/viewer", viewerRouter);
 
 app.listen(port, () => {
   console.log("App is running on port " + port);
