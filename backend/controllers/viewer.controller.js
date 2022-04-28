f954df68b3eb7a5b5674b2b5ac758ace5ab8ae23
David Padron Castillo <padroncastillo@gmail.com>
2022-04-28 13:59:12 -0400

    Some documentation

--- backend/controllers/viewer.controller.js
+++ backend/controllers/viewer.controller.js
@@ -1,13 +1,18 @@
 const Object3D = require("../models/object3D.model");
 
+// @desc    Get a list of all the objects
+// @route   GET /api/viewer/
+// @access  Public
 const getAllObjectData = async (req, res) => {
   const items = await Object3D.find();
   return res.send({ items: items });
 };
 
+// @desc    Get all the data for a particular object
+// @route   GET /api/viewer/:object_name
+// @access  Public
 const getObjectData = async (req, res) => {
   const object_name = req.params.name;
-  console.log(object_name);
   const item = await Object3D.find({ name: object_name });
   return res.send({
     items: item,
