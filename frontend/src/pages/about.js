f12ba70d3e01130a251cd719c3c1246735690f02
David Padron Castillo <padroncastillo@gmail.com>
2022-04-28 15:40:55 -0400

    Create object3DService for fetching objects

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import object3DService from "../services/object3DService";
 
 function About(props) {
-  return <h1> Hello {props.text} </h1>;
+  const [text, setText] = React.useState("");
+
+  useEffect(() => {
+    object3DService.getAllObjects().then((data) => {
+      console.log(data.items);
+      setText(data.items);
+    });
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      Hello {props.text} and data: {JSON.stringify(text)}
+    </div>
+  );
 }
 
 export default About;
